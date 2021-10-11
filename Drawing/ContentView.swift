8dd59e92a658ecd4c0a81d1e4fd153a44ce0354c
Yash Poojary <yashspoojary245@gmail.com>
2021-10-11 19:30:26 +0530

    Challenge solution

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,254 +7,155 @@
 
 import SwiftUI
 
-
-struct Spirograph: Shape {
-    let innerRadius: Int
-    let outerRadius: Int
-    let distance: Int
-    let amount: CGFloat
-    
-    func gcd(_ a: Int, _ b: Int) -> Int {
-        var a = a
-        var b = b
-        
-        if b != 0 {
-            let temp = b
-            b = b % a
-            a = temp
-        }
-        
-        return a
-    }
-    
-    func path(in rect: CGRect) -> Path {
-        let divisor = gcd(innerRadius, outerRadius)
-        let innerRadius = CGFloat(innerRadius)
-        let outerRadius = CGFloat(outerRadius)
-        let distance = CGFloat(distance)
-        let difference = innerRadius - outerRadius
-        let endPoint = ceil( 2 * CGFloat.pi * outerRadius / CGFloat(divisor)) * amount
-        
-        var path = Path()
-        
-        for theta in stride(from: 0, through: endPoint, by: 0.01) {
-            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
-            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
-            
-             x += rect.width / 2
-             y += rect.height / 2
-            
-             if theta == 0 {
-                path.move(to: CGPoint(x: x, y: y))
-            } else {
-                path.addLine(to: CGPoint(x: x, y: y))
-            }
-        }
-        return path
-    }
-    
-}
-
-struct ContentView: View {
-    @State private var innerRadius = 125.0
-    @State private var outerRadius = 75.0
-    @State private var distance = 25.0
-    @State private var amount: CGFloat = 1.0
-    @State private var hue = 0.6
-
-    
-    
+struct ColorCyclingRectangle: View {
+    var steps = 100
+    var amount = 0.0
     
     var body: some View {
-        VStack(spacing: 0){
-            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
-                .stroke(Color(hue: hue, saturation: 1, brightness: amount))
-                .frame(width: 300, height: 300)
-            
-            Spacer()
-            
-            Group {
-                Text("Inner Radius: \(Int(innerRadius))")
-                Slider(value: $innerRadius, in: 10...150, step: 1)
-                    .padding([.horizontal, .bottom])
-                
-                Text("Outer Radius: \(Int(outerRadius))")
-                Slider(value: $outerRadius, in: 10...150, step: 1)
-                    .padding([.horizontal, .bottom])
-                
-                Text("Distance: \(Int(distance))")
-                Slider(value: $distance, in: 10...150, step: 1)
-                    .padding([.horizontal, .bottom])
-                
-                Text("Amount: \(Int(amount))")
-                Slider(value: $distance, in: 10...150, step: 1)
-                    .padding([.horizontal, .bottom])
-                
-                
-                Text("Color")
-                Slider(value: $hue)
-                    .padding(.horizontal)
+        ZStack {
+            ForEach(0..<steps) { value in
+                Rectangle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        color(for: value, brightness: 1),
+                        color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
             }
-            
-            
         }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+       var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
 
 
+struct ContentView: View {
+    @State private var amount = 0.0
+    
+    var body: some View {
+        VStack {
+            ColorCyclingRectangle(amount: amount)
+                .frame(width: 300, height: 300)
+            
+            Slider(value: $amount)
+                .padding()
+        }
+    }
+}
+ 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
 
+
+
 //
-//struct CheckerBoard: Shape {
-//    var rows: Int
-//    var columns: Int
+//struct ArrowHead: Shape {
 //
-//     public var animatableData: AnimatablePair<Double, Double> {
-//        get {
-//            AnimatablePair(Double(rows), Double(columns))
-//        }
-//        set {
-//            rows = Int(newValue.first)
-//            columns = Int(newValue.second)
-//        }
-//    }
 //
 //    func path(in rect: CGRect) -> Path {
 //        var path = Path()
 //
-//        let rowSize = rect.height / CGFloat(rows)
-//        let columnSize = rect.width / CGFloat(columns)
+//        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+//        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY/4))
+//        path.addLine(to: CGPoint(x: rect.maxX/3, y: rect.maxY/4))
+//        path.addLine(to: CGPoint(x: rect.maxX/3, y: rect.maxY))
+//        path.addLine(to: CGPoint(x: rect.maxX/1.5, y: rect.maxY))
+//        path.addLine(to: CGPoint(x: rect.maxX/1.5, y: rect.maxY/4))
+//        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY/4))
+//        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
 //
 //
-//        for row in 0..<rows {
-//            for column in 0..<columns {
-//                if (row + column).isMultiple(of: 2) {
-//                    let startX = columnSize * CGFloat(column)
-//                    let startY = rowSize * CGFloat(row)
 //
-//                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
-//
-//                    path.addRect(rect)
-//                }
-//            }
-//        }
 //
 //        return path
 //
 //    }
 //}
 //
-//struct ContentView: View {
-//    @State private var rows = 4
-//    @State private var columns = 4
-//
-//    var body: some View {
-//        CheckerBoard(rows: rows, columns: columns)
-//            .onTapGesture {
-//                withAnimation(.linear(duration: 3)) {
-//                    rows = 16
-//                    columns = 16
-//                }
-//            }
-//
-//    }
-//}
-
-//struct Trapezoid: Shape {
-//    var insetAmount: CGFloat
-//
-//    var animatableData: CGFloat {
-//        get {  insetAmount }
-//        set {  insetAmount = newValue  }
-//    }
-//
-//
-//    func path(in rect: CGRect) -> Path {
-//        var path = Path()
-//
-//        path.move(to: CGPoint(x: 0, y: rect.maxY))
-//        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
-//        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
-//        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-//        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-//        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
 //
 //
 //
-//        return path
-//    }
-//}
 //
 //struct ContentView: View {
-//    @State private var amount: CGFloat = 50
-//
-//    var body: some View {
-//       Trapezoid(insetAmount: amount)
-//            .frame(width: 300, height: 300)
-//            .onTapGesture {
-//                withAnimation {
-//                    amount = CGFloat.random(in: 10...90)
-//                }
-//            }
-//
-//
-//    }
-//}
-
-
-
-//struct ContentView: View {
-//
-//    @State private var petalOffset = -20.0
-//    @State private var petalWidth = 100.0
+//    @State private var insetAmount: CGFloat = 80.0
+//    var lineWidth = 5
+//    @State private var isBold = false
 //
 //
 //    var body: some View {
 //        VStack {
-//            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-//                .fill(Color.red, style: FillStyle(eoFill: true))
-//
-//            Text("Offset")
-//            Slider(value: $petalOffset, in: -40...40)
-//                .padding([.horizontal, .bottom])
-//
-//            Text("Width")
-//            Slider(value: $petalWidth, in: 0...100)
-//                .padding(.horizontal)
+//            ArrowHead()
+//                .stroke(Color.red, style: StrokeStyle(lineWidth: CGFloat(isBold ? 20 : lineWidth), lineCap: .round, lineJoin: .round))
+//            .onTapGesture {
+//                withAnimation(.linear(duration: 1)) {
+//                    isBold.toggle()
+//                }
+//            }
+//            .frame(width: 150, height: 350)
+//            Spacer()
 //
 //
 //        }
-//    }
 //
+//    }
 //}
 
 
-
-
-//struct Flower: Shape {
-//    var petalOffset: Double = -20
-//    var petalWidth: Double = 100
 //
-//    func path(in rect: CGRect) -> Path {
-//        var path = Path()
+//struct ColorCyclingCircle: View {
+//    var amount = 0.0
+//    var steps = 100
 //
-//        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 3) {
-//            let rotation = CGAffineTransform(rotationAngle: number)
+//    var body: some View {
+//        ZStack {
+//            ForEach(0..<steps) { value in
+//                Circle()
+//                    .inset(by: CGFloat(value))
+//                    .strokeBorder(LinearGradient(colors: [
+//                        color(for: value, brightness: 1),
+//                        color(for: value, brightness: 0.5)
+//                    ], startPoint: .top, endPoint: .bottom), lineWidth: 10)
+//            }
+//        }
+//        .drawingGroup()
+//    }
 //
-//            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width/2, y: rect.height / 2))
+//    func color(for value: Int, brightness: Double) -> Color {
+//        var targetHue = Double(value) / Double(steps) + amount
 //
-//            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
-//
-//            let rotatedPetal = originalPetal.applying(position)
-//
-//            path.addPath(rotatedPetal)
+//        if targetHue > 1 {
+//            targetHue -= 1
 //        }
 //
-//        return path
+//        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+//    }
+//}
+
+
+//
+//struct ContentView: View {
+//    @State private var ColorCycle = 0.0
+//
+//
+//
+//    var body: some View {
+//        VStack {
+//            ColorCyclingCircle(amount: ColorCycle)
+//                .frame(width: 300, height: 300)
+//
+//            Slider(value: $ColorCycle)
+//        }
 //    }
 //}
